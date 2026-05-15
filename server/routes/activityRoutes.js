8213d309c4cafa551c9a32157cb6b3f4ddc86934
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-05-15 09:01:36 +0530

    Add activity routes with authentication

---
+++ server/routes/activityRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { getActivities } = require("../controllers/activityController");
+const { protect } = require("../middleware/auth");
+
+const router = express.Router();
+router.get("/", protect, getActivities);
+
+module.exports = router;
